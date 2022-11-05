393d6e6bfdac2b354e10a53dbabfe70aeb2ca251
GiuliaCasteluci <giulia.casteluci@mykenzie.snhu.edu>
2022-11-05 18:42:38 -0300

    not working

--- code.js
+++ code.js
@@ -1,1 +1,33 @@
-// Your code here
+
+const tenName = [
+  "",
+  "ten",
+  "twenty",
+  "thirty",
+  "forty",
+  "fifty",
+  "sixty",
+  "seventy",
+  "eighty",
+  "ninety",
+  "hundred"
+];
+
+function numbersToWords(){
+    const onesName = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+      ];
+    
+    let input= document.getElementById("numbers").value;
+    document.getElementById("numword").innerHTML=onesName[eval(input)];
+}
+

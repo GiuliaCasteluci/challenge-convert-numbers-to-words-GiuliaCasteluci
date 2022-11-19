5185645864dbb45cabb0babc39587ac1093ad0bd
kenzieadmin <kenzieadmin@users.noreply.github.com>
2022-11-19 22:40:02 +0000

    deploy: 31134d7487494c88f2ad25b3e47f1ffe070b585f

--- README.md
+++ README.md
@@ -8,8 +8,11 @@ Customize this README.md however you want to turn it into documentation for your
 
 _(Put your project plan here. It could be pseudocode, an outline-style development plan, etc. But whatever form you choose, it should be detailed enough that another developer could feasibly use it to implement your solution.)_
 
+First, for the basic part of the challenge 0-100. I create different arrays to storage the numbers that i wanted to convert into words. Secondly, after studying my options,  i realized that the best way was to use a if/else statment. I tried to use a switch case because for bigger function, it is preferable, but it just didn't work. Third, I started writing the if statment for example, if number is less than 0 return - my array of number, and i kept on writing 
+
 ## Reflection
 
 What different approaches or techniques did you consider when planning your implementation? What were the advantages and disadvantages of those alternatives?
 
 _(Put your reflection answer here.)_
+At first, i used  Conditional (ternary) operator for my statments, but it just made things complicated to read. So i changed that. Also, i had a lot of challenge making the if else statment work, a lot of debbuging. 
--- code.js
+++ code.js
@@ -1,52 +1,76 @@
-const tenName = [
-    "",
-    "ten",
-    "twenty",
-    "thirty",
-    "forty",
-    "fifty",
-    "sixty",
-    "seventy",
-    "eighty",
-    "ninety",
-    "hundred",
-  ];
-  const onesName = [
-    "zero",
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
-  ];
-  const illionsName = ["", "thousand", "million", "billion"];
-  
-  numberToWords = (number, string = "", showZero = true) => {
-    const count = String(Math.floor(number)).length;
-    const illionIndex = Math.floor((count - 1) / 3);
-    return number < 0
-      ? "negative" + numberToWords(-number)
-      : number == 0
-      ? showZero
-        ? "null"
-        : ""
-      : string +
-        (number < 20
-          ? onesName[number]
-          : number < 100
-          ? tenName[Math.floor(number / 10)] +
-            numberToWords(number % 10, " ", false)
-          : number < 1000
-          ? onesName[Math.floor(number / 100)] +
-            " hundred" +
-            numberToWords(number % 100, " and ", false)
-          : illionsName[illionIndex]);
-  };
-  
-  //The Math.floor() function always rounds down and returns the largest integer less than or equal to a given number.
-  
-  
+const onesName = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
+
+const tensName = [
+  "",
+  "",
+  "twenty",
+  "thirty",
+  "forty",
+  "fifty",
+  "sixty",
+  "seventy",
+  "eighty",
+  "ninety",
+  "hundred",
+];
+
+const xwy = [
+  "ten",
+  "eleven",
+  "twelve",
+  "Thirteen",
+  "Fifteen",
+  "Fourteen",
+  "Sixteen",
+  "Seventeen",
+  "Eighteen",
+  "Nineteen",
+];
+const number = 101;
+const illionsName = ["", "thousand", "million", "billion"];
+const count = String(Math.floor(number)).length;
+const illionIndex = Math.floor((count - 1) / 3);
+
+function numberToWords(number) {
+  if (number < 0) {
+    return "negative" + " " + numberToWords(-number);
+  } else if (number < 10) {
+    return onesName[number];
+  } else if (number < 20) {
+    return xwy[number.toString()[1]];
+  } else if (number < 100) {
+    return (
+      tensName[Math.floor(number / 10)] + " " + onesName[number.toString()[1]]
+    );
+  }
+}
+
+//funciton after 100 not working
+// function numberToWords(number) {
+//   number = 111
+//   if(number < 0) {
+//     "negative" + ' ' + numberToWords(-number);
+//   } else if(number < 10) {
+//     onesName[number];
+//   } else if(number <  20) {
+//     xwy[number.toString()[1]];
+//   } else if(number < 100) {
+//     tenName[Math.floor(number / 10)] + ' ' + onesName[number.toString()[1]];
+//   } else if(number < 1000) {
+
+//     onesName[Math.floor(number / 100)] +  " hundred" + ' ' + tenName[number.toString()[1]] + ' ' + onesName[number.toString()[2]];
+//   } else if(number > 1000) {
+//     'something'
+//   }
+// };

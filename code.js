f176f4e14fedc8c07f341b5ce5116989a6583a19
GiuliaCasteluci <giulia.casteluci@mykenzie.snhu.edu>
2023-01-04 17:15:25 -0300

    10.000 working

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 const onesName = [
-  "zero",
+  "",
   "one",
   "two",
   "three",
@@ -9,19 +9,16 @@ const onesName = [
   "seven",
   "eight",
   "nine",
-];
-
-const teens = [
   "ten",
   "eleven",
   "twelve",
-  "Thirteen",
-  "Fifteen",
-  "Fourteen",
-  "Sixteen",
-  "Seventeen",
-  "Eighteen",
-  "Nineteen",
+  "thirteen",
+  "fourteen",
+  "fifteen",
+  "sixteen",
+  "seventeen",
+  "eighteen",
+  "nineteen",
 ];
 
 const tensName = [
@@ -36,53 +33,41 @@ const tensName = [
   "eighty",
   "ninety",
 ];
-
 const illionsName = [
-  "hundred",
-  "thousand",
-  "million",
-  "billion"
+  '',
+  'hundred',
+  'thousand',
 ];
 
-
-const number = 101;
-const count = String(Math.floor(number)).length;
-const illionIndex = Math.floor((count - 1) / 3);
-
 function numberToWords(number) {
-  if (number < 0) {
-    return "negative" + " " + numberToWords(-number);
-  } else if (number < 10) {
-    return onesName[number];
-  } else if (number < 20) {
-    return teens[number.toString()[1]];
-  } else if (number < 101) {
-    return (
-      tensName[Math.floor(number / 10)] + " " + onesName[number.toString()[1]]
-    );
+  let index = number.toString().slice(-2)
+  let n =  index < 10 || index > 20 ? number.toString().slice(-1) : index
+  let word = '';
+  let count = String(Math.floor(number)).length;
+  let illionIndex = Math.floor((count - 1) / 3);
+  let hundreds = number.toString()[1] > 0 ? onesName[number.toString()[1]] + ' hundred' : ''
+  let decimals = number.toString()[2] > 0 ? tensName[number.toString()[2]] : ''
+
+  if (number == 0) {
+    return 'zero'
+  } 
+
+  if (number == 10000) {
+    return 'ten thousand'
   }
-}
 
-
-//funciton after 100 not working
-function number2(number) {
-  console.log(number)
   if (number < 0) {
-    return "negative" + ' ' + numberToWords(-number);
-  } else if (number < 10) {
-    return onesName[number];
-  } else if (number < 20) {
-    return teens[number.toString()[1]];
+    word = "negative" + ' ' + numberToWords(-number);
+  } else if (number < 20 && number > 0) {
+    word = onesName[number];
   } else if (number < 100) {
-    return tensName[Math.floor(number / 10)] + ' ' + onesName[number.toString()[1]];
+    word = tensName[Math.floor(number / 10)] + ' ' + onesName[number.toString()[1]];
   } else if (number < 1000) {
-    return onesName[Math.floor(number / 100)] + " hundred" + ' ' + tensName[number.toString()[1]] + ' ' + onesName[number.toString()[2]];
-  } else if (number > 999 && number < 10000000) {
-    //convert numbers > 1000 to words
-    // new logic for numbers > 999
-    //fix undefined
-    return  onesName[Math.floor(number / 1000)] + " thousand" + ' ' + tensName[number.toString()[1]] + ' ' + onesName[number.toString()[2]];
-  } else if (number < 1000) {
-    return onesName[Math.floor(number / 100)] + " hundred" + ' ' + tensName[number.toString()[1]] + ' ' + onesName[number.toString()[2]];
-}
+    word = onesName[Math.floor(number / 100)] + " hundred" + ' ' + tensName[number.toString()[1]] + ' ' + onesName[n];
+  } else if (number > 999 && number < 10000) {
+    word = onesName[Math.floor(number / 1000)] + ' ' + illionsName[illionIndex + 1] + ' ' + hundreds + ' ' + decimals + ' ' + onesName[n]
+  }
+
+  return word.trim()
 };
+  
